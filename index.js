5e65d48a3411b18102f4d83a07acc85d1ebac1b9
Purnabhishek Sripathi <76512306+abhisripathi@users.noreply.github.com>
2023-09-18 23:59:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 app.get('/itc505/homework/2', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
-
 app.listen(port, ()=> {
     console.log('Server started at http://localhost:' + port);
   });
